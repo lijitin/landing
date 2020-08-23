caf881907be1263bbdbeddf53c2b928f3262d03d
Justin <jli208@hawk.iit.edu>
2020-08-23 01:02:32 -0500

    sidebar update

--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
 import React from 'react';
 import './App.css';
+import headshot from './assets/headshot.png';
 
 function App() {
   return (
     <div className="App"> 
         <div className="main">
           <div className="side-bar">
-            {/* todo */}
-          </div>
+            <div className="circle-crop">
+              <img src={headshot} alt="profile_picture"></img>"
+            </div>
+            <div className="description">
+              <p>It's Justin</p>
+            </div>
+            <div className="links">
+              {/* todo */}
+            </div>
+            <div className="javascript-fun-stuffs">
+              {/* todo 
+                1. Get a spinning donut
+                2. Maybe do a clicker game of some sort?
+              */}
+            </div>
+          </div> 
           <div className="spotlight">
             {/* todo */}
           </div>
