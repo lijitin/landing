dd38e2365dace76cd810a8149f37b63ac73f5a00
Justin <jli208@hawk.iit.edu>
2020-08-24 02:06:36 -0500

    donut update

--- src/App.js
+++ src/App.js
@@ -1,37 +1,53 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 import './App.css';
 import headshot from './assets/headshot.png';
 
-function App() {
-  return (
-    <div className="App"> 
-        <div className="main">
-          <div className="side-bar">
-            <div className="circle-crop">
-              <img src={headshot} alt="profile_picture"></img>"
+import Contacts from './components/Contacts';
+import Donut from './components/Donut';
+
+class App extends React.Component {
+  render(){
+    return (
+      <div className="App"> 
+          <div className="main">
+            <div className="side-bar">
+              <div className="circle-crop">
+                <img src={headshot} alt="profile_picture"></img>"
+              </div>
+              <div className="description">
+                <p>Ji Tin Justin Li</p>
+              </div>
+              <div className="links">
+                {/* todo*/}
+                <Contacts></Contacts>
+              </div>
+              <div className="javascript-fun-stuffs">
+                <button onClick={this.handleClick.bind(this, 'donut')}>Spinny donut</button>
+                <button onClick={(e) => this.handleClick('clicker')}>Something-clicker</button>
+                {/* todo 
+                  1. Get a spinning donut
+                  2. Maybe do a clicker game of some sort?
+                */}
+              </div>
+            </div> 
+            <div className="display">
+                <div id="spotlight">
+
+                </div>
             </div>
-            <div className="description">
-              <p>It's Justin</p>
-            </div>
-            <div className="links">
-              {/* todo*/}
-              <p>Links will be added soon.</p>
-            </div>
-            <div className="javascript-fun-stuffs">
-              <p>Spinny donuts</p>
-              <p>Something-clicker</p>
-              {/* todo 
-                1. Get a spinning donut
-                2. Maybe do a clicker game of some sort?
-              */}
-            </div>
-          </div> 
-          <div className="spotlight">
-            {/* todo */}
           </div>
-        </div>
-    </div>
-  );
+      </div>
+    );
+  }
+  handleClick = (value) => {
+    if(value === 'donut'){
+      ReactDOM.render(<Donut />,document.getElementById("spotlight"));
+    }else if (value === 'clicker'){
+      /*todo*/
+    }
+    
+  }
 }
 
 export default App;
---
+++ src/components/Contacts.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import LinkedInLogo from '../assets/linkedin_light_32x32.png';
+import GitHubLogo from '../assets/GitHub-Mark-Light-32px.png';
+import MailLogo from '../assets/mail-32.png';
+import './Contacts.css';
+
+class Contacts extends React.Component {
+
+    render() {
+        return (
+            <div>
+                <div className="icons">
+                    <a href="mailto: lijitin11@gmail.com">
+                        <img src={MailLogo} alt="MailInLogo"></img>
+                    </a>
+                    <a href="https://www.linkedin.com/in/ji-tin-justin-li/">
+                        <img src={LinkedInLogo} alt="LinkedInLogo"></img>
+                    </a>
+                    <a href="https://github.com/lijitin/">
+                        <img src={GitHubLogo} alt="GithubLogo"></img>
+                    </a>
+                </div>
+            </div>
+        ); 
+    }
+
+}
+export default Contacts;
